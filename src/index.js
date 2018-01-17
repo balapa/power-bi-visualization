c302194f942398aaac0f3e8f76a008911f3b08a3
Bhakti Al Akbar <balapa95@gmail.com>
2018-01-17 20:24:30 +0700

    added chart2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Chart1 from './Chart1';
+import Chart1 from './components/Chart1';
+import Chart2 from './components/Chart2';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Chart1 />, document.getElementById('chart-wrapper'));
+ReactDOM.render(<Chart2 />, document.getElementById('chart2-wrapper'));
 registerServiceWorker();
