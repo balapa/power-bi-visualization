51f6f5fcdbc5e23e4a473820f449973047808c9c
Bhakti Al Akbar <balapa95@gmail.com>
2018-01-19 19:33:40 +0700

    added chart3 to index.js react

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Chart1 from './components/Chart1';
 import Chart2 from './components/Chart2';
+import Chart3 from './components/Chart3';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Chart1 />, document.getElementById('chart-wrapper'));
 ReactDOM.render(<Chart2 />, document.getElementById('chart2-wrapper'));
+ReactDOM.render(<Chart3 />, document.getElementById('chart3-wrapper'));
 registerServiceWorker();
