bc52163a5f8e3d2019448252eb7b38a6fb6dc8f6
Bhakti Al Akbar <balapa95@gmail.com>
2018-01-23 17:35:57 +0700

    1. added type filter such as (blank), products, and services 2. added legend list 3. added new json data for products and (blank)

--- src/components/Chart3.jsx
+++ src/components/Chart3.jsx
@@ -3,6 +3,8 @@ import createClass from "create-react-class";
 import "./../css/style.css";
 import data from "./../data/chart3.json";
 import dataServices from "./../data/chart3-services.json";
+import dataProducts from "./../data/chart3-products.json";
+import dataBlank from "./../data/chart3-blank.json";
 import _ from "lodash";
 
 const Chart3 = createClass({
@@ -13,23 +15,38 @@ const Chart3 = createClass({
 				item.isActive = true;
 				return item;
 			}),
-			maxFlight: _.maxBy(data, "flights").flights,
-			minFlight: _.minBy(data, "flights").flights,
-			maxQuantity: _.maxBy(data, "Quantity").Quantity,
-			minQuantity: _.minBy(data, "Quantity").Quantity,
 			airportCodes: _.uniqBy(data, (item)=> item.airport_code).map(item=> {
 				return {
 					content: item.airport_code,
 					isActive: true
 				}
 			}),
-			flightsRange: [],
+			yAxisArray: [0,1,2],
+			yAxisBase: 300,
+			yAxisBaseWidth: 125,
+			xAxisArray: [0,1,2],
+			xAxisBase: 2000,
+			xAxisBaseHeight: 125,
 			airportColors: {
-				"ATL": "#e74c3c",
+				"ATL": "rgb(239, 107, 93)",
 				"JFK": "#1abc9c",
 				"LAX": "#3498db",
 				"LGA": "#9b59b6"
-			}
+			},
+			types: [
+				{
+					content: "(Blank)",
+					isActive: false
+				},
+				{
+					content: "PRODUCTS",
+					isActive: false
+				},
+				{
+					content: "SERVICES",
+					isActive: false
+				}
+			]
 		}
 	},
 
@@ -58,103 +75,174 @@ const Chart3 = createClass({
 		});
 	},
 
+	handleType(e) {
+		const elValue = e.target.dataset.value;
+		let newType = {}
+		const newTypes = this.state.types.map(type=> {
+			if(type.content === elValue){
+				newType = {
+					content: type.content,
+					isActive: !type.isActive
+				}
+				return newType;
+			} else {
+				return {
+					content: type.content,
+					isActive: false
+				};
+			}
+		});
+
+		this.setState({
+			types: newTypes,
+			airportCodes: this.state.airportCodes.map(code=> {
+				return {
+					content: code.content,
+					isActive: true
+				}
+			})
+		});
+
+		if(newType.content === "SERVICES" && newType.isActive) {
+			this.setState({
+				yAxisBase: 100,
+				yAxisBaseWidth: 115,
+				yAxisArray: [0,1,2,3],
+				flights: dataServices.map(item=> {
+					item.isActive = true;
+					return item;
+				})
+			})
+		} else if(newType.content === "PRODUCTS" && newType.isActive) {
+			this.setState({
+				yAxisBase: 200,
+				yAxisBaseWidth: 115,
+				yAxisArray: [0,1,2,3],
+				flights: dataProducts.map(item=> {
+					item.isActive = true;
+					return item;
+				})
+			})
+		} else if(newType.content === "(Blank)" && newType.isActive) {
+			this.setState({
+				yAxisBase: 0.5,
+				yAxisBaseWidth: 115,
+				yAxisArray: [0,1,2,3],
+				flights: dataBlank.map(item=> {
+					item.isActive = true;
+					return item;
+				})
+			})
+		} else {
+			this.setState({
+				yAxisBase: 300,
+				yAxisBaseWidth: 125,
+				yAxisArray: [0,1,2],
+				flights: data.map(item=> {
+					item.isActive = true;
+					return item;
+				})
+			})
+		}
+	},
+
 	render() {
 
 		return (
 			<div>
 
-				<div className="checkbox-filters-wrapper">
-					{/* <div className="col-md-6 col-sm-12 col-xs-12"> */}
-
-				<div className="checkbox-filter">
-					<h3>Airport Code</h3>
-					<div className="airport-codes">
-						{
-							this.state.airportCodes.map((code, index)=> {
-
-								let codeClass = "";
-								(code.isActive) ? codeClass = "code-item active" : codeClass = "code-item";
-
-								return <div key={index} data-value={code.content} className={codeClass} onClick={(e)=> this.handleCode(e)}>
-									<div className="code-checkbox">
-
-										<svg width="15" height="15" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
-											<path fill="none" className="checkmark" stroke="#FFF" strokeWidth="2" d="M2 7l4 4 7-7"/>
-										</svg>
-
-									</div>
-									<div className="code-content">{code.content}</div>
-								</div>
-							})
-						}
-					</div>
+				<div className="legend-list">
+					{
+						Object.keys(this.state.airportColors).map((key, index)=> {
+							return <div key={index} className="legend">
+								<div className="legend-ball" style={{ background: this.state.airportColors[key] }}></div>
+								<div className="legend-text">{key}</div>
+							</div>
+						})
+					}
 				</div>
 
-					{/* </div> */}
+				<div className="checkbox-filters-wrapper">
 
-					{/* <div className="col-md-6 col-sm-12 col-xs-12"> */}
-						<div className="checkbox-filter">
-							<h3>Type</h3>
-							<div className="types">
-								<div className="code-item" onClick={(e)=> {
+					<div className="checkbox-filter">
+						<h3>Airport Code</h3>
+						<div className="airport-codes display-flex">
+							{
+								this.state.airportCodes.map((code, index)=> {
 
-									e.target.classList.toggle("active");
+									let codeClass = "";
+									(code.isActive) ? codeClass = "code-item active" : codeClass = "code-item";
 
-									this.setState({
-										flights: dataServices.map(item=> {
-											item.isActive = true;
-											return item
-										})
-									})
+									return <div key={index} data-value={code.content} className={codeClass} onClick={(e)=> this.handleCode(e)}>
+										<div className="code-checkbox">
 
-								}}>
-									<div className="code-checkbox">
-										<svg width="15" height="15" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
-											<path fill="none" className="checkmark" stroke="#FFF" strokeWidth="2" d="M2 7l4 4 7-7"/>
-										</svg>
+											<svg width="15" height="15" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+												<path fill="none" className="checkmark" stroke="#FFF" strokeWidth="2" d="M2 7l4 4 7-7"/>
+											</svg>
+
+										</div>
+										<div className="code-content">{code.content}</div>
 									</div>
-									<div className="code-content">Services</div>
-								</div>
-							</div>
+								})
+							}
 						</div>
-					{/* </div> */}
+					</div>
+
+					<div className="checkbox-filter">
+						<h3>Type</h3>
+						<div className="types display-flex">
+
+							{
+								this.state.types.map((type, index)=> {
+
+									let elClass = "";
+									(type.isActive) ? elClass = "code-item active" : elClass = "code-item";
+
+									return <div key={index} data-value={type.content} className={elClass} onClick={(e)=> this.handleType(e)}>
+										<div className="code-checkbox">
+
+											<svg width="15" height="15" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+												<path fill="none" className="checkmark" stroke="#FFF" strokeWidth="2" d="M2 7l4 4 7-7"/>
+											</svg>
+
+										</div>
+										<div className="code-content">{type.content}</div>
+									</div>
+
+								})
+							}
+						</div>
+					</div>
 
 				</div>
 
 				<div className="chart">
 
 					<div className="x-axis">
-						<div className="line">
-							<div className="line-content">6K</div>
-						</div>
-						<div className="line">
-							<div className="line-content">4K</div>
-						</div>
-						<div className="line">
-							<div className="line-content">2K</div>
-						</div>
+						{
+							this.state.xAxisArray.map((item, index, arr)=> {
+								return <div className="line" key={index} style={{ height: this.state.xAxisBaseHeight }}>
+									<div className="line-content">{(arr.length - index) * this.state.xAxisBase}</div>
+								</div>
+							})
+						}
 					</div>
 
 					<div className="y-axis">
-						<div className="line">
-							<div className="line-content">300</div>
-						</div>
-						<div className="line">
-							<div className="line-content">600</div>
-						</div>
-						<div className="line">
-							<div className="line-content">900</div>
-						</div>
-						<div className="line">
-							<div className="line-content">1200</div>
-						</div>
+						{
+							this.state.yAxisArray.map((item, index, arr)=> {
+								return <div className="line" key={index} style={{ width: this.state.yAxisBaseWidth }}>
+									<div className="line-content">{(index + 1) * this.state.yAxisBase}</div>
+								</div>
+							})
+						}
 
 						{
 							this.state.flights.map((flight, index, arr)=> {
 
 								const ballStyle = {
-									left: flight.Quantity / 2.5,
-									bottom: flight.flights / 16
+									left: flight.Quantity / (this.state.yAxisBase / this.state.yAxisBaseWidth),
+									bottom: flight.flights / (this.state.xAxisBase / this.state.xAxisBaseHeight)
 								}
 
 								if(flight.airport_code === "JFK") {
@@ -175,13 +263,18 @@ const Chart3 = createClass({
 									flightBallClass = "flight-ball hidden";
 								}
 
-								return <div key={index} className={flightBallClass} style={ballStyle}></div>
+								if(Number.isInteger(flight.Quantity)) {
+									return <div key={index} className={flightBallClass} style={ballStyle}></div>
+								}
+
+								return undefined;
 							})
 						}
 
 					</div>
 
 				</div>
+
 			</div>
 		);
 	}
