f83904f9b6be1b248646ae0a46639e1a9dabe8c8
Bhakti Al Akbar <balapa95@gmail.com>
2018-02-13 01:11:12 +0700

    Delete Chart1.test.js

--- src/Chart1.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Chart1 from './Chart1';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Chart1 />, div);
-});
